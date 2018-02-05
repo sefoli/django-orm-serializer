7df1a37267f7b5a496699baeb83c5f85d276a4fb
Sefa <sefisakgms@gmail.com>
2018-02-05 18:01:26 +0300

    Bug fixed

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'ormserializer',
-  packages = ['ormserializer'], # this must be the same as the name above
+  name = 'django-orm-serializer',
+  scripts = ['ormserializer.py'], # this must be the same as the name above
   version = '0.1',
-  description = 'This serializer support django org data to python object',
+  description = 'This serializer support django orm data to python object',
   author = 'Sefa Akgumus',
   author_email = 'sefisakgms@gmail.com',
-  url = 'https://github.com/pyankara/', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  url = 'https://github.com/sefoli/', # use the URL to the github repo
+  download_url = 'https://github.com/sefoli/django-orm-serializer/archive/master.zip', # I'll explain this in a second
   keywords = ['django', 'serializer', 'json', 'xml'], # arbitrary keywords
   classifiers = [],
 )
