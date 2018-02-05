0c6e3e004c327e3967324040cbee6c169b968caa
Sefa <sefisakgms@gmail.com>
2018-02-05 17:12:51 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ormserializer',
+  packages = ['ormserializer'], # this must be the same as the name above
+  version = '0.1',
+  description = 'This serializer support django org data to json or xml',
+  author = 'Sefa Akgumus',
+  author_email = 'sefisakgms@gmail.com',
+  url = 'https://github.com/pyankara/', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['django', 'serializer', 'json', 'xml'], # arbitrary keywords
+  classifiers = [],
+)
